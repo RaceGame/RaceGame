0649fda052bdbcaad8bd84f618d00a7391cb7cda
Kevin <D183028@edu.rocwb.nl>
2018-11-20 09:12:53 +0100

    de game
    2 mappen, rijdende auto, steady camera

---
+++ RaceGame/Assets/scripts/Stable_Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stable_Camera : MonoBehaviour {
+
+    public GameObject TheCar;
+    public float CarX;
+    public float CarY;
+    public float CarZ;
+
+	// Update is called once per frame
+	void Update () {
+        CarX = TheCar.transform.eulerAngles.x;
+        CarY = TheCar.transform.eulerAngles.y;
+        CarZ = TheCar.transform.eulerAngles.z;
+
+
+        //makes sure the cube to keep facing us.
+        transform.eulerAngles = new Vector3(CarX - CarX, CarY, CarZ - CarZ);
+
+
+    }
+}
